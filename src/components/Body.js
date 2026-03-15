f75d69740b2f801892a2701c4fe0aea89cb0b16e
Kartik Sundriyal <kartiksundriyal2004@gmail.com>
2026-03-15 16:19:46 +0530

    Header done, useState used for getting the array

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,10 +1,16 @@
+import { useEffect, useState } from "react";
+
 function Body(){
-    return (
-        <>
-         
+    const [profile,setProfile]=useState([]);
+    async function GenerateProfiles(){
+        const resp=await fetch("https://api.github.com/users?per_page=10");
+        const data=await resp.json();
+        setProfile(data);
+    }
+    useEffect(()=>{
+        <GenerateProfiles/>
+    },[])
 
-
-        </>
-    )
+   
 }
 export default Body;
